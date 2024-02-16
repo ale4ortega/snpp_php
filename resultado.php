1dafe2b86484e5af455924cc5c009b712b59b4e4
ale4ortega <92593455+ale4ortega@users.noreply.github.com>
2024-02-16 16:21:59 -0300

    Update resultado.php
    Se han añadido ifs para casos de error

--- resultado.php
+++ resultado.php
@@ -15,25 +15,30 @@ function multiplicar($x, $y){
 function dividir($x, $y){
     return $x/$y;
 }
+
+
 //Variable global
+if (isset($_GET["num1"]) && isset($_GET["num2"]) && isset($_GET["op"])){
     $numero1 = $_GET["num1"];
     $numero2 = $_GET["num2"];
     $operacion = $_GET["op"];
     
+    if (!empty($numero1) && !empty($numero2) && !empty($operacion)) {
+        if ($operacion == "suma"){
+            $resultado = sumar($numero1, $numero2);
+        }else if ($operacion == "resta"){
+            $resultado = restar($numero1, $numero2);
+        }else if ($operacion == "multiplicacion"){
+            $resultado = multiplicar($numero1, $numero2);
+        }else if ($operacion == "division"){
+            $resultado = dividir($numero1, $numero2);
+        }
 
-if ($operacion == "suma"){
-    $resultado = sumar($numero1, $numero2);
-}else if ($operacion == "restar"){
-    $resultado = restar($numero1, $numero2);
-}else if ($operacion == "multiplicacion"){
-    $resultado = multiplicar($numero1, $numero2);
-}else if ($operacion == "division"){
-    $resultado = dividir($numero1, $numero2);
+        echo "El resultado es: ".$resultado;
+
+    }else {
+        echo "Algún campo está vacío";
+    }
+
 }
-
-echo "El resultado es: ".$resultado;
-
-
-
-
 ?>
